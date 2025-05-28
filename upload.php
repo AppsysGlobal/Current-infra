b20cf19ef3696374c59bc79f03b20f4a69f70b80
AppsysGlobal <sajith.m@appsysglobal.com>
2025-05-28 13:28:37 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,9 +1,11 @@
 <?php
+putenv("HOME=/var/www");
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $target = "/tmp/" . basename($_FILES['file']['name']);
     if (move_uploaded_file($_FILES['file']['tmp_name'], $target)) {
         echo "File uploaded. Uploading to OCI...<br>";
-        $cmd = "oci os object put -bn bucket-20250521-1835 --file {$target} --namespace idyhabl91i8j";
+        $cmd = "sudo /usr/bin/oci os object put -bn bucket-20250521-1835 --file {$target} --namespace idyhabl91i8j";
         echo "<pre>" . shell_exec($cmd) . "</pre>";
     } else {
         echo "Upload failed.";
